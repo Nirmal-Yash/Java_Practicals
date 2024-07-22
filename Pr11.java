c90972792c4e6a0616ef89d7b16b394ebea746df
Nirmal-Yash <123491131+Nirmal-Yash@users.noreply.github.com>
2024-07-22 18:26:54 +0530

    Add files via upload

---
+++ Pr11.java
@@ -0,0 +1,8 @@
+class Pr11{
+	public static void main(String[] args){
+		String str = "CHARUSAT UNIVERSITY";
+		System.out.println("The Length of String is :"+str.length());
+		System.out.print("The Replaced String :" + str.replace('H','Y'));
+		System.out.println("The String in LowerCase :" + str.toLowerCase() );
+	}
+}
